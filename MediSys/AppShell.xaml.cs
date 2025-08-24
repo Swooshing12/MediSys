6fa8918478c0aa91edb42a470d95937feb73b2b5
Swooshing12 <swooshing14@gmail.com>
2025-08-24 13:37:44 -0500

    Agregar archivos de proyecto.

---
+++ MediSys/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MediSys
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MediSys/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MediSys
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
